fae9e5590261fba540235d580fd3e0d6839b464c
Yuya Ochiai <yuya0321@gmail.com>
2017-01-13 21:51:22 +0900

    Fix arg for v3.6.0

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export FILE_SETTINGS__AMAZON_S3_BUCKET=${FILE_SETTINGS__AMAZON_S3_BUCKET:=""}
 
 lib/envsubst < config/config-heroku-template.json > config/config-heroku.json
 
-bin/platform -config=config/config-heroku.json
+bin/platform --config=config/config-heroku.json
